095ff4564ddaeb1af4cccc949efbd7826edaf520
carlosArruda1996 <126702417+carlosArruda1996@users.noreply.github.com>
2023-05-26 21:45:33 -0300

    Button.java

--- src/Vision/Button.java
+++ src/Vision/Button.java
@@ -1,2 +1,15 @@
-package Vision;public class Button {
+package Vision;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Button extends JButton {
+    public Button(String label, Color color){
+        setText(label);
+        setBackground(color);
+        setForeground(Color.WHITE);
+        setOpaque(true);
+        setFont(new Font("sans serif",Font.PLAIN,25));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
 }
