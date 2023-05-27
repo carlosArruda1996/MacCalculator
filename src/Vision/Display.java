b4414efec44f4171bcd3768fac804b43c890ebb8
carlosArruda1996 <126702417+carlosArruda1996@users.noreply.github.com>
2023-05-26 21:47:46 -0300

    Display.java

--- src/Vision/Display.java
+++ src/Vision/Display.java
@@ -1,2 +1,28 @@
-package Vision;public class Display {
+package Vision;
+
+import Model.Memory;
+import Model.ObserverMemory;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JPanel implements ObserverMemory {
+
+    private final JLabel label = new JLabel(Memory.getInstance().getCurrentText());
+
+    Display(){
+        Memory.getInstance().addObserver(this);
+        setBackground(new Color(46,49,50));
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("sans serif",Font.PLAIN,35));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 8, 18));
+
+        add(label);
+    }
+
+    @Override
+    public void newValue(String newValue) {
+        label.setText(newValue);
+    }
 }
